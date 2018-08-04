d4703962205c459fe7897af8fdbddcbe907adb8d
alelax <alessandro.lausdei@gmail.com>
2018-08-04 19:24:31 +0200

    improved view

--- resources/views/userLogged/userPanel.blade.php
+++ resources/views/userLogged/userPanel.blade.php
@@ -4,39 +4,38 @@
 @section('content')
 
 
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">                
-                <div class="panel-heading">
-                    Your Apartment
-                    <div class="apartments-list-cnt" >
-                        {{-- Apartments list --}}
-                        @if(!empty($apartments))
-                            @foreach($apartments as $apartment)
-                                <div class="card{{ !($apartment->is_active) ? ' disabled-card' : null }}" style="width: 18rem;">
-                                    <img class="img-responsive thumbnail" src="{{ asset('storage/'.$apartment->thumbnail) }}" alt="Card image cap">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $apartment->title }}</h5>
-                                        <a href=" {{ route('ownerApartmentDetails', $apartment->id) }} " class="btn btn-primary">Visualizza dettagli</a>
-                                        <a class="btn btn-danger delete-id" data-route-delete="{{ route('apartaments.destroy', $apartment->id) }}" href="#">Elimina</a> 
-                                        <form action="{{ route('apartaments.update', $apartment->id) }}" method="post">
-                                            {{ csrf_field() }}
-                                            {{ method_field('PUT') }}
-                                            <input type="hidden" name="isActive" value="{{ $apartment->is_active ? 1 : 0 }}" class="secret">    
-                                            <input type="submit" value="{{ $apartment->is_active ? 'Disattiva annuncio' : 'Attiva annuncio' }}" class="switch btn btn-warning">
-                                        </form>
-                                    </div>
-                                </div>   
-                            @endforeach
-                        @endif
-                
-                        <div class="panel-body">
-                            <button id="addApartment">+</button>
-                        </div>
-                    </div>
-                </div>                
-            </div>      
+        <div class="col-md-10 col-md-offset-1 section_cnt">                           
+            <div id="section_title">Your Apartments</div> 
+            <div class="apartments-list-cnt">
+                {{-- Apartments list --}}
+                @if(!empty($apartments))
+                    @foreach($apartments as $apartment)
+                        <div class="card{{ !($apartment->is_active) ? ' disabled-card' : null }}">
+                            <div class="img_cnt">
+                                <img class="img-responsive" src="{{ asset('storage/'.$apartment->thumbnail) }}" alt="Card image cap">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $apartment->title }}</h5>
+                                <a href=" {{ route('ownerApartmentDetails', $apartment->id) }} " class="btn custom_btn_show">Visualizza dettagli</a>
+                                <a class="btn btn-danger delete-id custom_btn_delete" data-route-delete="{{ route('apartaments.destroy', $apartment->id) }}" href="#">Elimina</a> 
+                                <form action="{{ route('apartaments.update', $apartment->id) }}" method="post">
+                                    {{ csrf_field() }}
+                                    {{ method_field('PUT') }}
+                                    <input type="hidden" name="isActive" value="{{ $apartment->is_active ? 1 : 0 }}" class="secret">    
+                                    <input type="submit" value="{{ $apartment->is_active ? 'Disattiva annuncio' : 'Attiva annuncio' }}" class="switch btn custom_btn_disable">
+                                </form>
+                            </div>
+                        </div>   
+                    @endforeach
+                @else
+                    <div>Resgistra un appartamento</div>
+                @endif            
+                <div class="card add_btn">
+                    <button id="addApartment"><span>+</span></button>
+                </div>
+            </div>                  
         </div>    
     </div>
 
