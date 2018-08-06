1fa18fcc4656f7e321d720c68ba5059416325782
alelax <alessandro.lausdei@gmail.com>
2018-08-06 13:48:50 +0200

    new migrations

--- resources/views/components/slide_menu.blade.php
+++ resources/views/components/slide_menu.blade.php
@@ -1,13 +1,13 @@
 <div class="slide_menu d_none">
     @if (Route::has('login'))
         @auth
-            <a href="{{ route('home') }}">Profile</a>
-            <a href="{{ route('inbox.show') }}">Inbox</a>  
             <a href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
                 Logout
             </a>
+            <a href="{{ route('inbox.show') }}">Inbox</a>  
+            <a href="{{ route('home') }}">Profile</a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 {{ csrf_field() }}
             </form>
